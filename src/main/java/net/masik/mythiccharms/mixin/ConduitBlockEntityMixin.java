d1825ed079646e70948a37bd10d098bf3842e509
Maks <maksim.volokitin.05@gmail.com>
2024-08-25 16:16:10 -0700

    v. 1.1.4
    fix conflict with other mods using Redirect

--- src/main/java/net/masik/mythiccharms/mixin/ConduitBlockEntityMixin.java
+++ src/main/java/net/masik/mythiccharms/mixin/ConduitBlockEntityMixin.java
@@ -1,20 +1,21 @@
 package net.masik.mythiccharms.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.masik.mythiccharms.util.CharmHelper;
 import net.minecraft.block.entity.ConduitBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.FluidTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ConduitBlockEntity.class)
 public class ConduitBlockEntityMixin {
 
-    @Redirect(method = "givePlayersEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
-    private static boolean inject(PlayerEntity instance) {
+    @WrapOperation(method = "givePlayersEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
+    private static boolean drownedFreedomEffectConduit(PlayerEntity instance, Operation<Boolean> original) {
 
-        if (!CharmHelper.charmDrownedFreedomEquipped(instance)) return instance.isTouchingWaterOrRain();
+        if (!CharmHelper.charmDrownedFreedomEquipped(instance)) return original.call(instance);
 
         return instance.updateMovementInFluid(FluidTags.WATER, 0.014);
 
--- src/main/java/net/masik/mythiccharms/mixin/EnderDragonEntityMixin.java
+++ src/main/java/net/masik/mythiccharms/mixin/EnderDragonEntityMixin.java
@@ -1,29 +1,30 @@
 package net.masik.mythiccharms.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.masik.mythiccharms.util.CharmHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
-import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Iterator;
 
 @Mixin(EnderDragonEntity.class)
 public class EnderDragonEntityMixin {
 
-    @Redirect(method = "launchLivingEntities", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;"))
-    private Object mountainsStrengthEffectDragon(Iterator instance) {
+    @WrapOperation(method = "launchLivingEntities", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;"))
+    private Object mountainsStrengthEffectDragon(Iterator instance, Operation<Object> original) {
 
         Entity entity = (Entity) instance.next();
 
-        if (!entity.isPlayer()) return(entity);
+        if (!entity.isPlayer()) return original.call(instance);
 
-        if (!CharmHelper.charmMountainsStrengthEquipped((LivingEntity) entity)) return(entity);
+        if (!CharmHelper.charmMountainsStrengthEquipped((LivingEntity) entity)) return original.call(instance);
 
         return null;
+
     }
 
 }
